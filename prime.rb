7d149e5176a3dae7cb1eb393574c977fe727279c
Chris Cardle <cdcardle@gmail.com>
2017-11-01 15:26:10 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,13 @@
-def prime?(n)
+require 'benchmark'
+
+
+Benchmark.bm do |x|
+  x.report { def prime?(n)
   return false if n <= 1
   return true if (n == 2 || n == 3)
   return false if (n % 2 == 0 || n % 3 == 0)
   x = Math.sqrt(n)
   (2..x).each {|i| return false if n % i == 0}
   return true
+end }
 end
