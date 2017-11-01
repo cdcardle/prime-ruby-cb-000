2fea25f720971fe70ae90e510c6db2c400a59d5c
Chris Cardle <cdcardle@gmail.com>
2017-11-01 15:18:41 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -6,12 +6,34 @@ def prime?(n)
   elsif (n % 2 == 0 || n % 3 == 0)
     return false
   else
-    (2..(n - 1)).each do |i|
+    (2..(Math.sqrt(n).floor)).each do |i|
       if n % i == 0
-        return false
-      else
-        return true
+        false
       end
+      return true
     end
   end
+end
+
+def prime(n)
+  i = 2
+  while i <= Math.sqrt(n)
+    if n % i == 0
+      false
+      i += 1
+    end
+    return true
+  end
+end
+
+
+def is_prime?(n)
+  return false if n <= 1
+  return true if (n == 2 || n == 3)
+  i = 2
+  while i < n
+    return false if num % i == 0
+    i += 1   
+  end
+  return true
 end
