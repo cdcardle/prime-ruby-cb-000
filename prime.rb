fabb91c051600663a68bce621f0e3add625eda46
Chris Cardle <cdcardle@gmail.com>
2017-11-01 15:22:59 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,10 +2,7 @@ def prime?(n)
   return false if n <= 1
   return true if (n == 2 || n == 3)
   return false if (n % 2 == 0 || n % 3 == 0)
-  i = 2
-  while i < Math.sqrt(n)
-    return false if n % i == 0
-    i += 1
-  end
+  x = Math.sqrt(n)
+  (2..x).each {|i| return false if n % i == 0}
   return true
 end
